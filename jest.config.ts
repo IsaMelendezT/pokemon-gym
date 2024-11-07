9db7aa227bac24363d3956f5e43d2f5461b800a7
IsaMelendezT <157085293+IsaMelendezT@users.noreply.github.com>
2024-11-06 19:56:36 -0500

    Adding ts-jest dependencies

--- jest.config.ts
+++ jest.config.ts
@@ -1,4 +1,12 @@
-export default {
-    preset: 'ts-jest',
-    testEnvironment: 'node',
-  };
+// jest.config.ts
+import type { Config } from '@jest/types';
+
+const config: Config.InitialOptions = {
+  preset: 'ts-jest',                  // Use ts-jest preset for TypeScript
+  testEnvironment: 'node',            // Use Node.js environment for testing
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',         // Transform .ts and .tsx files using ts-jest
+  },
+};
+
+export default config;
